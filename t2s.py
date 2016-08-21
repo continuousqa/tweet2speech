d4150d40d172443d57c81fc54d1df797efc51a31
cq <continuousqa@gmail.com>
2016-08-21 12:32:49 -0700

    Added menu driven system
    Added menu and some exception handling

--- t2s.py
+++ t2s.py
@@ -2,17 +2,42 @@ from bs4 import BeautifulSoup
 import urllib
 from gtts import gTTS
 import re
+import sys
 
 dialogue = []
 def twitterAccountFeed(feed):
     html = urllib.urlopen('https://twitter.com/'+feed)
+    if html.getcode() == 200 and feed != '':
+        parseTweets(html, feed)
+    else:
+        print "Invalid Twitter Feed"
+        sys.exit(1)
+
+def parseTweets(html, feed):
     bt = BeautifulSoup(html)
     for div in bt.findAll("div", {"class":"js-tweet-text-container"}):
         noUrl = re.sub(r'\w+:\/{2}[\d\w-]+(\.[\d\w-]+)*(?:(?:\/[^\s/]*))*', '', div.text) # code from http://stackoverflow.com/questions/11331982/how-to-remove-any-url-within-a-string-in-python
         dialogue.append(noUrl)
     speech = ''.join(dialogue)
-    tts = gTTS(text=speech, lang='en')
-    tts.save(feed + '.mp3')
+    try:
+        tts = gTTS(text=speech, lang='en')
+        tts.save(feed + '.mp3')
+        print "Twitter feed has been converted to an mp3"
+    except Exception, e:
+        print "Error on audio conversion: "
+        print e
+    # return menu()  # Use if you want to return to the menu after completion.
 
+def menu():
+    print " "
+    print "/-- Tweet2Speech - Converts a user's Twitter feed to a spoken mp3 file --\\"
+    print " "
+    print "    [*] Be sure to have gTTS and bs4 modules installed before running"
+    print "    [*] Input a twitter handle without the @ (i.e. DalaiLama)"
+    print "    [*] An mp3 file will be output in the home folder"
+    print "  "
+    feed = raw_input("Enter the Twitter Name you want to capture (i.e. DalaiLama) : ")
+    twitterAccountFeed(feed)
 
-twitterAccountFeed("DalaiLama")
+if __name__ == '__main__':
+    menu()
